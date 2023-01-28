c4281b56b8bdfa0d3fae804f5774f7d1fb3a00e5
Neal <33704976+nealmick@users.noreply.github.com>
2023-01-28 18:02:27 -0500

    Update settings.py

--- bb/settings.py
+++ bb/settings.py
@@ -132,7 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 CRIPSPY_TEMPLATE_PACK = 'bootstrap4'
-LOGIN_REDIRECT_URL = 'home-page'
+LOGIN_REDIRECT_URL = 'home-predict'
 LOGIN_URL = 'login'
 
 
