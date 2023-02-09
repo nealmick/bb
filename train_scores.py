542412b1370d85c1120db8f4156a9d0f6c54f990
Neal <33704976+nealmick@users.noreply.github.com>
2023-02-09 12:32:04 -0500

    Update train_scores.py

--- train_scores.py
+++ train_scores.py
@@ -6,7 +6,8 @@ from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoi
 from sklearn.model_selection import train_test_split
 import datetime
 
-path = "csv/test123.csv"
+path = "csv/train.csv"
+test_path = "csv/test.csv"
 current_time = str(time.time())
 
 data = pd.read_csv(path)
@@ -19,16 +20,15 @@ data.drop(['home_score', 'visitor_score', 'gameid'], axis=1, inplace=True)
 data = data.values
 data = data.astype(float)
 
-x_train, x_test, y_train, y_test = train_test_split(data, np.column_stack((homeScore, visitorScore)), test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(data, np.column_stack((homeScore, visitorScore)), test_size=0.0001)
 
 #x_train = tf.keras.utils.normalize(x_train, axis=1)
 #x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.Sequential([
 
-    tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(512, activation='LeakyReLU'),
+    tf.keras.layers.Dense(256, activation='LeakyReLU'),
     
     tf.keras.layers.Dense(2, activation='linear'),
 
@@ -40,34 +40,41 @@ log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 
-
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=75, validation_split=0.1, batch_size=16,callbacks=[tensorboard_callback],shuffle=True)
+#creating and training model then saving
+model.compile(optimizer='adamax', loss='mean_squared_error', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=30, validation_split=0.1, batch_size=16 ,callbacks=[tensorboard_callback],shuffle=True)
 model.save_weights('./checkpoints/my_checkpoint')
 
 
-def print_predictions(model, x_test, y_test):
-    y_pred = model.predict(x_test)
+'''
+##testinged model
+data = pd.read_csv(test_path)
+homeTestScore = data['home_score'].values
+visitorTestScore = data['visitor_score'].values
+data.drop(['gameid'], axis=1, inplace=True)
+data.drop(['home_score'], axis=1, inplace=True)
+data.drop(['visitor_score'], axis=1, inplace=True)
+
+data = data.values
+data = data.astype(float)
+
+def print_prediction(model,data):
+    p = model.predict(data)
     c = 0
-    total = 0
-    for i in range(len(y_pred)):
-        print("Predicted home score: {:.2f} | Actual home score: {:.2f}".format(y_pred[i][0], y_test[i][0]))
-        print("Predicted visitor score: {:.2f} | Actual visitor score: {:.2f}".format(y_pred[i][1], y_test[i][1]))
-
-        if y_pred[i][0] > y_pred[i][1] and y_test[i][0] > y_test[i][1]:
-
-            print(round(float(y_pred[i][0]) - float(y_pred[i][1])),"--------------correct-------------",round(float(y_test[i][0]) - float(y_test[i][1])))
-            
+    n = 0
+    for i in range(len(p)):
+        correct = False
+        n+=1
+        if p[i][0] > p[i][1] and homeTestScore[i] > visitorTestScore[i]:
+            correct = True
+        elif p[i][0] < p[i][1] and homeTestScore[i] < visitorTestScore[i]:
+            correct = True
+        if correct:
             c+=1
-        elif y_pred[i][0] < y_pred[i][1] and y_test[i][0] < y_test[i][1]:
-            print(round(float(y_pred[i][1]) - float(y_pred[i][0])), "--------------correct-------------",round(float(y_test[i][1]) - float(y_test[i][0])))
+            print('correct - predicted:',round(p[i][0]),round(p[i][1]),'=',round(p[i][0]-p[i][1]),' actual:' ,homeTestScore[i],visitorTestScore[i],'=',round(homeTestScore[i]-visitorTestScore[i]))
+        if not correct:
+            print('wrong - predicted:',round(p[i][0]),round(p[i][1]),'=',round(p[i][0]-p[i][1]),' actual:' ,homeTestScore[i],visitorTestScore[i],'=',round(homeTestScore[i]-visitorTestScore[i]))
 
-
-            c+=1
-        else:
-            print(round(float(y_pred[i][0]) - float(y_pred[i][1])),"--------------wrong-------------",round(float(y_test[i][0]) - float(y_test[i][1])))
-
-        total +=1
-    print("correct %:  ", (c/total)*100)
-
-print_predictions(model, x_test, y_test)
+    print('percent correct winners: ', c/n*100,'%')
+print_prediction(model, data)
+'''
