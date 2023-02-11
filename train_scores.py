8739ddf907269ce6923827be258080105b45043e
Neal <33704976+nealmick@users.noreply.github.com>
2023-02-11 11:29:59 -0500

    Update train_scores.py

--- train_scores.py
+++ train_scores.py
@@ -27,8 +27,8 @@ x_train, x_test, y_train, y_test = train_test_split(data, np.column_stack((homeS
 
 model = tf.keras.Sequential([
 
-    tf.keras.layers.Dense(512, activation='LeakyReLU'),
-    tf.keras.layers.Dense(256, activation='LeakyReLU'),
+    tf.keras.layers.Dense(64, activation='LeakyReLU'),
+    tf.keras.layers.Dense(32, activation='LeakyReLU'),
     
     tf.keras.layers.Dense(2, activation='linear'),
 
@@ -39,18 +39,20 @@ model = tf.keras.Sequential([
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-
+#model.load_weights('./checkpoints/my_checkpoint')
 #creating and training model then saving
-model.compile(optimizer='adamax', loss='mean_squared_error', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=30, validation_split=0.1, batch_size=16 ,callbacks=[tensorboard_callback],shuffle=True)
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=50, validation_split=0.2, batch_size=16 ,callbacks=[tensorboard_callback],shuffle=True)
 model.save_weights('./checkpoints/my_checkpoint')
 
 
-'''
+
 ##testinged model
 data = pd.read_csv(test_path)
 homeTestScore = data['home_score'].values
 visitorTestScore = data['visitor_score'].values
+spread = data['spread'].values
+
 data.drop(['gameid'], axis=1, inplace=True)
 data.drop(['home_score'], axis=1, inplace=True)
 data.drop(['visitor_score'], axis=1, inplace=True)
@@ -60,21 +62,61 @@ data = data.astype(float)
 
 def print_prediction(model,data):
     p = model.predict(data)
-    c = 0
-    n = 0
+    c = 0#count correct winners
+    n = 0#count all
+    s = 0#count spread correct winner
+    ev = 0#count expected value
     for i in range(len(p)):
-        correct = False
+        correct = False#if prediction is correct winner
+        spreadCorrect = False#if spread is correct winner
+        pmscore = round(homeTestScore[i]-visitorTestScore[i]) #plus minus score
+        pmp = p[i][0]-p[i][1]
         n+=1
+        spread[i]=spread[i]*-1
+
+        if spread[i]>0 and homeTestScore[i]>visitorTestScore[i]:
+            spreadCorrect = True
+            s +=1
+        if spread[i]<0 and homeTestScore[i]<visitorTestScore[i]:
+            s +=1
+            spreadCorrect = True
+
         if p[i][0] > p[i][1] and homeTestScore[i] > visitorTestScore[i]:
             correct = True
         elif p[i][0] < p[i][1] and homeTestScore[i] < visitorTestScore[i]:
             correct = True
+        spreadError = abs(spread[i]-pmscore)
+        predictionError = abs(pmp-pmscore)
+        '''
+        if not correct and spreadError > predictionError:
+            correct=True
+        '''
+
         if correct:
             c+=1
-            print('correct - predicted:',round(p[i][0]),round(p[i][1]),'=',round(p[i][0]-p[i][1]),' actual:' ,homeTestScore[i],visitorTestScore[i],'=',round(homeTestScore[i]-visitorTestScore[i]))
-        if not correct:
-            print('wrong - predicted:',round(p[i][0]),round(p[i][1]),'=',round(p[i][0]-p[i][1]),' actual:' ,homeTestScore[i],visitorTestScore[i],'=',round(homeTestScore[i]-visitorTestScore[i]))
+
+        print('spread:',spreadCorrect,spread[i], 'prediction: ',correct,round(p[i][0]),round(p[i][1]),'=',round(p[i][0]-p[i][1]),' actual:' ,homeTestScore[i],visitorTestScore[i],'=',pmscore)
+
+
+        '''
+        spreadError = abs(spread[i]-pmscore)
+        predictionError = abs(pmp-pmscore)
+
+        print('spread error:', spreadError,'prediction error:', predictionError)
+
+        if spreadError < predictionError:
+            print(' game lost aginst spread.   currently at', round(ev/n*100),'%')
+
+        else:
+            ev+=1
+            print('game won against spread. currently at', round(ev/n*100),'%')
+
+        '''
+
+        print('#-------------------------------------------#')
 
     print('percent correct winners: ', c/n*100,'%')
+    print('spread percent correct winners: ', s/n*100,'%')
+    #print('expected value: ', ev/n*100,'%')
 print_prediction(model, data)
-'''
+
