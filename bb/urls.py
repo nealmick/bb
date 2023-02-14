69d24cb38f25e6eb0fcce96730c2db490cee9aa0
Neal <33704976+nealmick@users.noreply.github.com>
2023-02-14 11:34:52 -0500

    Update urls.py

--- bb/urls.py
+++ bb/urls.py
@@ -1,27 +1,14 @@
-"""bb URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from predict import views as predict_views
 urlpatterns = [
     path('', include('etc.urls')),
+    path('export/', predict_views.exportGames , name='export-games'),
     path('admin/', admin.site.urls),
     path('predict/', include('predict.urls')),
     path('register/', user_views.register, name='register'),
